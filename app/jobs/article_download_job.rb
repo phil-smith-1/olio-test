9662aa0a9a785950cef5661dbaf68977191f0ae4
Phillip Smith <pcsmith86@gmail.com>
2023-06-17 09:39:45 +0100

    Improve article download job creation/update process

--- app/jobs/article_download_job.rb
+++ app/jobs/article_download_job.rb
@@ -24,24 +24,24 @@ class ArticleDownloadJob
     stored_articles = Article.all
     articles.each do |article|
       stored_article = stored_articles.find { |item| item.id == article['id'] }
-      if stored_article && stored_article.updated_at < article['updated_at']
-        stored_article.update(
-          title: article['title'],
-          description: article['description'],
-          section: article['section'],
-          created_at: article['created_at'],
-          updated_at: article['updated_at']
-        )
-      elsif !stored_article
-        Article.create(
-          id: article['id'],
-          title: article['title'],
-          description: article['description'],
-          section: article['section'],
-          created_at: article['created_at'],
-          updated_at: article['updated_at']
-        )
+      if process_article?(article, stored_article)
+        Article.upsert(article_map(article))
       end
     end
+  end
+
+  def process_article?(article, stored_article)
+    !stored_article || (stored_article && stored_article.updated_at < article['updated_at'])
+  end
+
+  def article_map(article)
+    {
+      id: article['id'],
+      title: article['title'],
+      description: article['description'],
+      section: article['section'],
+      created_at: article['created_at'],
+      updated_at: article['updated_at']
+    }
   end
 end
